3bc07d833052524df80cd63959404869b9fadf1b
ckjwww <ckjwww@empal.com>
2021-06-13 00:53:26 +0900

    File upload function

--- src/main/java/com/godcoder/myhome/controller/BaseBallController.java
+++ src/main/java/com/godcoder/myhome/controller/BaseBallController.java
@@ -11,11 +11,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.godcoder.myhome.service.baseballService;
 import com.godcoder.myhome.service.codeService;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.awt.*;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -29,22 +33,14 @@ public class BaseBallController  implements WebMvcConfigurer {
     codeService codeservice;
 
     @GetMapping("/listposition")
-    public String listPosition(Model model
-                            , @RequestParam(required = false) String manageid) {
+    public String listPosition(Model model) {
 
         baseballDTO baseballEmptyDto = new baseballDTO();
-        baseballDTO baseballDto = null;
-        if (manageid == null) {
-            baseballDto = new baseballDTO();
-        } else {
-            baseballDto = baseballservice.retrieveBaseball(manageid);
-        }
 
         List<baseballDTO> baseballistDto = baseballservice.retrieveBaseballist();
         List<codeDTO> codeDto = codeservice.retrieveCode("01");
 
         model.addAttribute("baseball", baseballEmptyDto);
-        model.addAttribute("baseballOne", baseballDto);
         model.addAttribute("baseballist", baseballistDto);
         model.addAttribute("pos", codeDto);
 
@@ -55,31 +51,46 @@ public class BaseBallController  implements WebMvcConfigurer {
     public String baseballmodal(Model model
             , @RequestParam(required = false) String manageid) {
 
-        baseballDTO baseballEmptyDto = new baseballDTO();
         baseballDTO baseballDto = null;
+
         if (manageid == null) {
             baseballDto = new baseballDTO();
         } else {
-            baseballDto = baseballservice.retrieveBaseball(manageid);
+            baseballDto = baseballDto = baseballservice.retrieveBaseball(manageid);;
         }
 
-        List<baseballDTO> baseballistDto = baseballservice.retrieveBaseballist();
         List<codeDTO> codeDto = codeservice.retrieveCode("01");
 
-        model.addAttribute("baseball", baseballEmptyDto);
         model.addAttribute("baseballOne", baseballDto);
-        model.addAttribute("baseballist", baseballistDto);
         model.addAttribute("pos", codeDto);
 
         return "baseball/baseballmodal";
     }
 
     @PostMapping("/listposition")
-    public String listPosition(@Valid @ModelAttribute("baseballDto") baseballDTO baseballDto, Model model) {
+    public String listPosition(@Valid @ModelAttribute("baseballDto") baseballDTO baseballDto
+                             //, @RequestPart MultipartFile files
+                             //,  @RequestParam("files") MultipartFile files
+                             , MultipartHttpServletRequest files
+                             //, HttpServletRequest files
+                             , Model model) {
 
         if (baseballDto.getCheckBox() == null) {
             baseballDto.setCheckBox("N");
         }
+
+        MultipartFile mfile = files.getFile("files");
+        String orgFileName = mfile.getOriginalFilename();
+        long fileSize = mfile.getSize();
+        String contentType = mfile.getContentType();
+
+
+//        Iterator iter = files.getFileNames();
+//        while (iter.hasNext()) {
+//            fieldName = iter.next().toString(); // 내용을 가져와서
+//             mfile = files.getFile(fieldName);
+//
+//        }
 
         if (baseballDto.getManageId().isEmpty()) {
             HashMap hashValue = baseballservice.insertBaseball(baseballDto);
@@ -87,11 +98,6 @@ public class BaseBallController  implements WebMvcConfigurer {
         } else {
             baseballservice.updateBaseball(baseballDto);
         }
-
-        //List<codeDTO> codeDto = codeservice.retrieveCode("01");
-
-        //model.addAttribute("baseball", baseballDto);
-        //model.addAttribute("pos", codeDto);
 
         return "redirect:/baseball/listposition";
     }
