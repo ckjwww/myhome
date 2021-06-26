c98a3e70a1d4612ae828cce741e5bb2d140fbbd5
ckjwww <ckjwww@empal.com>
2021-06-26 13:05:25 +0900

    grid test

---
+++ src/main/resources/AUIGrid/AUIGridLicense.js
@@ -0,0 +1,9 @@
+/*!
+ * AUIGrid 3.0.10 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1
+ * Expiration Date : 2021-07-24
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+AUIGridLicense = "eyJjdCI6ImUxR1o5bEZOeUZYRVNSNk14NGpwMWErNlBJT04zeUp0N01uR3VDNjczMXdkeERoYWl2dmVpQjFpbTdtZUJuYVU3cUdCVHZNQ1pQbDlcL05Ca0NPSktFMWhDZ2lLVkZORUpqK3kwZ0xQTWJTdz0iLCJpdiI6Ijg3Y2YzZDM3MTQxNDFlMDZhZmM3ZTk5ZDU1Mjk0ZDhkIiwicyI6IjgxZWI4ZmE2NzM5YWY0ZTkifQ==";
